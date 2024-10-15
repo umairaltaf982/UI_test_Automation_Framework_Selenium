b996260cdf36365c3c215c4e1df8b784017c9868
umairaltaf982 <umairaltaf982@gmail.com>
2024-10-15 22:57:03 +0500

    Stared a project with maven and added dependencies with pom.xml and bdd gerkin with cucumber and junit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+    import org.openqa.selenium.By;
+    import org.openqa.selenium.WebDriver;
+    import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.findElement(By.cssSelector("#APjFqb")).sendKeys("Christiano Ronaldo");
+        driver.findElement((By.cssSelector("input[value='Google Search']"))).submit();
+    }
+
+}
---
+++ src/test/java/stepDefinition/LoginSteps.java
@@ -0,0 +1,29 @@
+package stepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+    @Given("User is on login page")
+    public void user_is_on_login_page() {
+        System.out.println("Inside Step - user is on login page");
+    }
+
+    @When("User enters username and password")
+    public void user_enters_username_and_password() {
+        System.out.println("Inside Step - user enter username and password");
+    }
+
+    @And("click on login button")
+    public void click_on_login_button() {
+        System.out.println("Inside Step - clicks the login button");
+    }
+
+    @Then("User is navigated to home page")
+    public void user_is_navigated_to_home_page() {
+        System.out.println("Inside Step - navigates to the home page");
+    }
+
+}
