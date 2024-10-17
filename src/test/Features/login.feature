4239430e5d8399d8e4bb1eb0736cdde515630d38
umairaltaf982 <umairaltaf982@gmail.com>
2024-10-18 00:25:50 +0500

    Run a proper login code of daraz.pk

--- src/test/Features/login.feature
+++ src/test/Features/login.feature
@@ -1,19 +1,20 @@
 Feature: feature to test login functionality
+
   Scenario: Check login is successful with valid credentials
     Given User is on login page
     When User enters username and password
     And click on login button
     Then User is navigated to home page
 
-  Scenario Outline: Check login is successful with valid credentials
-    Given User is on login page
-    When User enters <username> and <password>
-    And click on login button
-    Then User is navigated to home page
-
-    Examples:
-      | username | password |
-      | user1    | pass1    |
-      | user2    | pass2    |
-      | user3    | pass3    |
-      | user4    | pass4    |
+#  Scenario Outline: Check login is successful with valid credentials
+#    Given User is on login page
+#    When User enters <username> and <password>
+#    And click on login button
+#    Then User is navigated to home page
+#
+#    Examples:
+#      | username | password |
+#      | user1    | pass1    |
+#      | user2    | pass2    |
+#      | user3    | pass3    |
+#      | user4    | pass4    |
--- src/test/java/stepDefinition/LoginSteps.java
+++ src/test/java/stepDefinition/LoginSteps.java
@@ -4,27 +4,41 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class LoginSteps {
+    WebDriver driver = new ChromeDriver();
+    String url = "https://member.daraz.pk/user/login?redirect=https%3A%2F%2Fmember.daraz.pk%2Fuser%2Fprofile";
+
     @Given("User is on login page")
     public void user_is_on_login_page() {
-        System.out.println("Inside Step - user is on login page");
+        driver.get(url);
     }
 
     @When("User enters username and password")
     public void user_enters_username_and_password() {
-        System.out.println("Inside Step - user enter username and password");
+        WebElement email = driver.findElement(By.cssSelector("input[type='text']"));
+        email.sendKeys("f223737@cfd.nu.edu.pk");
+        WebElement password = driver.findElement(By.cssSelector("input[type='password']"));
+        password.sendKeys("abc@12345");
     }
 
     @And("click on login button")
     public void click_on_login_button() {
-        System.out.println("Inside Step - clicks the login button");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
     }
 
     @Then("User is navigated to home page")
     public void user_is_navigated_to_home_page() {
-        System.out.println("Inside Step - navigates to the home page");
+        if (!driver.getCurrentUrl().equals(url)) {
+            user_enters_username_and_password();
+        } else {
+            return;
+        }
     }
 
 }
