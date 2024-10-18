ddf693f038818da0bc6e0b5e1924f45b96bd89ec
umairaltaf982 <umairaltaf982@gmail.com>
2024-10-18 23:19:16 +0500

    added file beforeCheckout

--- src/test/java/stepDefinition/SearchSteps.java
+++ src/test/java/stepDefinition/SearchSteps.java
@@ -17,7 +17,7 @@ import java.io.ByteArrayInputStream;
 
 public class SearchSteps extends BaseTest {
 
-    String url = "https://member.daraz.pk/user/profile#/";
+    String url = "https://www.khazanay.pk/";
 
     @Before
     public void setUpScenario() {
@@ -31,19 +31,19 @@ public class SearchSteps extends BaseTest {
 
     @When("User enters search creditentials")
     public void user_enters_search_creditentials() {
-        WebElement search = driver.findElement(By.cssSelector("input[type='search']"));
-        search.sendKeys("winter gloves");
+        WebElement search = driver.findElement(By.cssSelector("#shopify-section-header-sidebar > div > div > div > div.t4s-search-header__form-wrap.t4s-text-center.t4s-d-none.t4s-d-lg-block.t4s-col-6.t4s-col-item.is--inted > form > div > input"));
+        search.sendKeys("sneakers for men");
     }
 
     @And("click on search button")
     public void click_on_search_button() {
-        WebElement searchBtn = driver.findElement(By.className("search-box__button--1oH7"));
+        WebElement searchBtn = driver.findElement(By.cssSelector("#shopify-section-header-sidebar > div > div > div > div.t4s-search-header__form-wrap.t4s-text-center.t4s-d-none.t4s-d-lg-block.t4s-col-6.t4s-col-item.is--inted > form > div > button"));
         searchBtn.click();
     }
 
     @Then("User is navigated to product related detail page")
     public void user_is_navigated_to_product_related_detail_page() {
-        
+
     }
 
     @After
